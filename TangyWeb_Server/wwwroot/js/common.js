86894ce64b18f0f66a480f86fdafbc4d1223934e
Raymond van Hoorn <vanhoornraymond@gmail.com>
2022-03-30 18:41:34 +0200

    Sectie 10: Delete Component

--- TangyWeb_Server/wwwroot/js/common.js
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -23,3 +23,11 @@ window.ShowSwal = (type, message) => {
         )
     }
 }
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
