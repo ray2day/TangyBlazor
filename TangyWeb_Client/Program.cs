0b84bf166a570d513d9d16504ce465b559626656
Raymond van Hoorn <vanhoornraymond@gmail.com>
2022-04-28 18:00:57 +0200

    Sectie 15: Shopping Cart and Summary

--- TangyWeb_Client/Program.cs
+++ TangyWeb_Client/Program.cs
@@ -4,6 +4,7 @@ using TangyWeb_Client;
 using TangyWeb_Client.Service;
 using TangyWeb_Client.Service.IService;
 using System.Globalization;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,7 +12,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("BaseAPIUrl")) });
 builder.Services.AddScoped<IProductService,ProductService>();
-
+builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddBlazoredLocalStorage();
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 
 await builder.Build().RunAsync();
