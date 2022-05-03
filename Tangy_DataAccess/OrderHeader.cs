8bde029730e478e87d39bd66ca452a9def68c78c
Raymond van Hoorn <vanhoornraymond@gmail.com>
2022-05-03 16:58:11 +0200

    Sectie 16: Order

---
+++ Tangy_DataAccess/OrderHeader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_DataAccess
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        // add navigation property : #TODO
+
+        [Required]
+        public double OrderTotal { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        public DateTime ShippingDate { get; set; }
+        [Required]
+        public string Status { get; set; }
+
+        // stripe payment
+        public string? SessionID { get; set; }
+        public string? PaymentIntentId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
---
+++ Tangy_Models/OrderDetailDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models
+{
+    public class OrderDetailDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public int OrderHeaderId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        public virtual ProductDTO Product { get; set; }
+
+        [Required]
+        public int Count { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public string Size { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+    }
+}
