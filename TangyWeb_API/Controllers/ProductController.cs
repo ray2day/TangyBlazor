bb5179e76da0016170d304b03ef597c48aa97625
Raymond van Hoorn <vanhoornraymond@gmail.com>
2022-05-07 13:28:52 +0200

    Sectie 18: Authorization in API

--- TangyWeb_API/Controllers/ProductController.cs
+++ TangyWeb_API/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Tangy_Business.Repository.IRepository;
+using Tangy_Common;
 using Tangy_Models;
 
 namespace TangyWeb_API.Controllers
---
+++ Tangy_Models/SignInRequestDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models
+{
+    public class SignInRequestDTO
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
